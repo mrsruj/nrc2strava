15936a90d9b822f349c7d4998dd759c82eeafea8
srujanpatil <patilsrujan7@gmail.com>
2020-01-09 13:02:44 +0530

    initial commit

---
+++ extract.py
@@ -0,0 +1,33 @@
+import json 
+from datetime import datetime
+
+def extract(filename):
+	"""
+	Extract data from json.
+
+	:param time: filepath to json 
+	:type time: str
+
+	:return: (time, name, [timestamps], [latitude], [longitude])
+	:rtype: tuple
+	"""
+	with open(filename, 'r') as f:
+		data = json.load(f)
+
+	if(data['type']=='run'):
+		timestamps, lat, lon = [], [], []
+		try:
+			time = datetime.utcfromtimestamp(data['start_epoch_ms']/1000).replace(microsecond=0).isoformat()+'Z'
+			name = data['tags']['com.nike.name']
+			for metric in data['metrics']:
+				if(metric['type']=='latitude'):
+					lat_values = metric['values']
+				elif(metric['type']=='longitude'):
+					lon_values = metric['values']
+			for lat1, lon1 in zip(lat_values, lon_values):
+				timestamps.append(datetime.utcfromtimestamp(lat1['start_epoch_ms']/1000).replace(microsecond=0).isoformat()+'Z')
+				lat.append(lat1['value'])
+				lon.append(lon1['value'])
+			return time, name, timestamps, lat, lon;
+		except:
+			return None
--- gpx_builder.py
+++ gpx_builder.py
@@ -1,48 +1,48 @@
 from xml.etree.ElementTree import Element, SubElement, tostring
 from xml.dom import minidom
+from slugify import slugify
 
-"""
-Build strava-compatible gpx file.
+def gpx_gen(time,name,timestamps,lat,lon):
+	"""
+	Build strava-compatible gpx file.
 
-:param time: activity start time 
-:type time: str (ISO 8061-extended)
+	:param time: activity start time 
+	:type time: str (ISO 8061-extended)
 
-:param name: name of activity
-:type name: str
+	:param name: name of activity
+	:type name: str
 
-:param timestamps: list of gps timestamps
-:type timestamps: [str]
+	:param timestamps: list of gps timestamps
+	:type timestamps: [str]
 
-:param lat: list of latitude values
-:type lat: [str]
+	:param lat: list of latitude values
+	:type lat: [str]
 
-:param lon: list of gps timestamps
-:type lon: [str]
+	:param lon: list of gps timestamps
+	:type lon: [str]
 
-:return: xml string
-:rtype: str
+	:return: xml string
+	:rtype: str
+	"""
+	Gpx = Element('gpx',{'creator':'StravaGPX','xmlns:xsi':'http://www.w3.org/2001/XMLSchema-instance','xsi:schemaLocation':'http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd','version':'1.1','xmlns':'http://www.topografix.com/GPX/1/1'})
 
-"""
+	Metadata = SubElement(Gpx,'metadata')
+	Time = SubElement(Metadata,'time')
+	Time.text = str(time)
+	Trk = SubElement(Gpx,'trk')
+	Name = SubElement(Trk,'name')
+	Name.text = str(name)
+	Type = SubElement(Trk,'type')
+	Type.text = '9'
+	Trkseg = SubElement(Trk,'trkseg')
 
-def gpx_gen(time, name, timestamps, lat, lon):
-  # Configure one attribute with set()
-  Gpx = Element('gpx',{'creator':'StravaGPX','xmlns:xsi':'http://www.w3.org/2001/XMLSchema-instance','xsi:schemaLocation':'http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd','version':'1.1','xmlns':'http://www.topografix.com/GPX/1/1'})
+	for i in range(len(timestamps)):
+		Trkpt = SubElement(Trkseg,'trkpt',{'lat':str(lat[i]),'lon':str(lon[i])})
+		tim = SubElement(Trkpt,'time')
+		tim.text = str(timestamps[i])
 
-  Metadata = SubElement(Gpx,'metadata')
-  Time = SubElement(Metadata,'time')
-  Time.text = str(time)
-  Trk = SubElement(Gpx,'trk')
-  Name = SubElement(Trk,'name')
-  Name.text = str(name)
-  Type = SubElement(Trk,'type')
-  Type.text = '9'
-  Trkseg = SubElement(Trk,'trkseg')
-
-  for i in range(len(timestamps)):
-    Trkpt = SubElement(Trkseg,'trkpt',{'lat':str(lat[i]),'lon':str(lon[i])})
-    time = SubElement(Trkpt,'time')
-    time.text = str(timestamps[i])
-
-  ugly_xml = tostring(Gpx, 'utf-8')
-  pretty_xml = minidom.parseString(ugly_xml)
-  return pretty_xml.toprettyxml(indent="  ")
+	ugly_xml = tostring(Gpx, 'utf-8')
+	pretty_xml = minidom.parseString(ugly_xml)
+	xml = pretty_xml.toprettyxml(indent="  ")
+	with open(join('./strava_gpx',slugify(name)+'_'+time+'.gpx'),'w') as f:
+		f.write(xml)
